8138fb35260af1ae88e99c927bd4234c7c8f3489
Timur Alperovich <timuralp@swiftstack.com>
2018-04-26 16:48:18 -0700

    Improve unit test coverage of the __main__ file.
    Improves the coverage of the branches in the __main__.main() function.
    Added tests for the proxy settings, as well as the "once" command line
    argument.

--- test/unit/test_main.py
+++ test/unit/test_main.py
@@ -22,6 +22,13 @@ import unittest
 
 class TestMain(unittest.TestCase):
 
+    def setUp(self):
+        # avoid loading boto3 and SyncContainer
+        sys.modules['s3_sync.sync_container'] = mock.Mock()
+
+    def tearDown(self):
+        del sys.modules['s3_sync.sync_container']
+
     @mock.patch('s3_sync.daemon_utils.os.path.exists')
     @mock.patch('s3_sync.daemon_utils.logging')
     @mock.patch('s3_sync.__main__.ContainerCrawler')
@@ -41,26 +48,68 @@ class TestMain(unittest.TestCase):
              'args': ['--log-level', 'debug'],
              'expected': 'DEBUG'},
         ]
+        defaults = ['main', '--console', '--conf', '/sample/config']
 
-        try:
-            # avoid loading boto3 and SyncContainer
-            sys.modules['s3_sync.sync_container'] = mock.Mock()
-            defaults = ['main', '--console', '--conf', '/sample/config']
+        for params in test_params:
+            with mock.patch('s3_sync.daemon_utils.load_config') \
+                    as conf_mock, \
+                    mock.patch('s3_sync.daemon_utils.sys') as sys_mock:
+                sys_mock.argv = defaults + params['args']
 
-            for params in test_params:
-                with mock.patch('s3_sync.daemon_utils.load_config') \
-                        as conf_mock, \
-                        mock.patch('s3_sync.daemon_utils.sys') as sys_mock:
-                    sys_mock.argv = defaults + params['args']
+                conf_mock.return_value = {}
+                if params['conf_level']:
+                    conf_mock.return_value['log_level'] = \
+                        params['conf_level']
 
-                    conf_mock.return_value = {}
-                    if params['conf_level']:
-                        conf_mock.return_value['log_level'] = \
-                            params['conf_level']
+                s3_sync.__main__.main()
+            mock_logger.setLevel.assert_has_calls(
+                [mock.call(params['expected'])] * 3)
+            mock_logger.reset_mock()
 
-                    s3_sync.__main__.main()
-                mock_logger.setLevel.assert_has_calls(
-                    [mock.call(params['expected'])] * 3)
-                mock_logger.reset_mock()
-        finally:
-            del sys.modules['s3_sync.sync_container']
+    @mock.patch('s3_sync.__main__.os')
+    @mock.patch('s3_sync.__main__.setup_logger')
+    @mock.patch('s3_sync.__main__.setup_context')
+    @mock.patch('s3_sync.__main__.ContainerCrawler')
+    def test_proxy_settings(self, crawler_mock, context_mock, logger_mock,
+                            os_mock):
+        test_params = [
+            {'http_proxy': 'http://some-proxy:1234'},
+            {'https_proxy': 'https://some-proxy:1443'}
+        ]
+
+        mock_args = mock.Mock(console=False, log_level='debug', once=False)
+
+        for conf in test_params:
+            context_mock.return_value = (mock_args, conf)
+            os_mock.environ = {}
+            s3_sync.__main__.main()
+            if 'http_proxy' in conf:
+                self.assertEqual(
+                    conf['http_proxy'], os_mock.environ['http_proxy'])
+                self.assertNotIn('https_proxy', os_mock.environ)
+            else:
+                self.assertEqual(
+                    conf['https_proxy'], os_mock.environ['https_proxy'])
+                self.assertNotIn('http_proxy', os_mock.environ)
+            context_mock.reset_mock()
+
+    @mock.patch('s3_sync.__main__.setup_logger')
+    @mock.patch('s3_sync.__main__.setup_context')
+    @mock.patch('s3_sync.__main__.ContainerCrawler')
+    def test_run_once(self, crawler_mock, context_mock, logger_mock):
+        tests = [mock.Mock(console=False, log_level='debug', once=False),
+                 mock.Mock(console=False, log_level='debug', once=True)]
+
+        # avoid loading boto3 and SyncContainer
+        sys.modules['s3_sync.sync_container'] = mock.Mock()
+        for test in tests:
+            context_mock.return_value = (test, {})
+            mocked_crawler = crawler_mock.return_value
+            s3_sync.__main__.main()
+            if test.once:
+                mocked_crawler.run_once.assert_called_once_with()
+            else:
+                mocked_crawler.run_always.assert_called_once_with()
+
+            crawler_mock.reset_mock()
+            context_mock.reset_mock()
